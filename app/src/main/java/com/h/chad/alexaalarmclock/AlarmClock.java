570beeddaad8086f4b0a57dea33c6bc5f9991efd
Chad H <chadg1980@gmail.com>
2017-03-14 15:34:41 -0700

    Added the FAB to the Alarm Clock to add a new alarm. Created a activity Voice Recorder to record a voice.

--- app/src/main/java/com/h/chad/alexaalarmclock/AlarmClock.java
+++ app/src/main/java/com/h/chad/alexaalarmclock/AlarmClock.java
@@ -1,13 +1,24 @@
 package com.h.chad.alexaalarmclock;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AlarmClock extends AppCompatActivity {
 
+    FloatingActionButton fab;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_clock);
+        setupFAB();
+
+    }
+
+    private void setupFAB() {
+        fab = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+
     }
 }
---
+++ app/src/main/java/com/h/chad/alexaalarmclock/VoiceRecorder.java
@@ -0,0 +1,17 @@
+package com.h.chad.alexaalarmclock;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chad on 3/14/2017.
+ */
+
+public class VoiceRecorder extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
