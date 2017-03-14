4c4a2c745c199f517827fa95eeb54939a3b96fa2
Chad H <chadg1980@gmail.com>
2017-03-14 11:32:29 -0700

    Original Commit, all code was generated by Android Studio

---
+++ app/src/main/java/com/h/chad/alexaalarmclock/AlarmClock.java
@@ -0,0 +1,13 @@
+package com.h.chad.alexaalarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmClock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_clock);
+    }
+}
