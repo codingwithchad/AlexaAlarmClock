8467299855a0cce58df470f66594d071822188c8
Chad H <chadg1980@gmail.com>
2017-03-17 13:47:57 -0700

    Added a TimePicker Fragment Added an icon for playback

---
+++ app/src/main/java/com/h/chad/alexaalarmclock/TimePickerFragment.java
@@ -0,0 +1,47 @@
+package com.h.chad.alexaalarmclock;
+
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.app.Dialog;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Calendar;
+
+/**
+ * Created by chad on 3/16/2017.
+ */
+
+public class TimePickerFragment extends DialogFragment
+    implements TimePickerDialog.OnTimeSetListener{
+
+    private TextView mTvHours;
+    private TextView mTvMinutes;
+
+    //constructor is empty
+    public TimePickerFragment(){
+
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //User current time as default
+        final Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        //Create and return a new instace of TimePickerDialog
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+        mTvHours = (TextView) getActivity().findViewById(R.id.tv_hours);
+        mTvMinutes = (TextView)getActivity().findViewById(R.id.tv_minutes);
+        mTvHours.setText(String.valueOf(hour)+":");
+        mTvMinutes.setText(String.valueOf(min));
+
+    }
+}
