5a473e9cbef48a2d82f97f61042e375a66de7d0f
Chad H <chadg1980@gmail.com>
2017-03-18 13:37:48 -0700

    created an array to hold the days of the week. (0 for off, 1 for on) I am saving the array as a string, I need to seperate the string at the comma when they come out of the database.

--- app/src/main/java/com/h/chad/alexaalarmclock/VoiceRecorder.java
+++ app/src/main/java/com/h/chad/alexaalarmclock/VoiceRecorder.java
@@ -1,11 +1,15 @@
 package com.h.chad.alexaalarmclock;
 
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Color;
 
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.os.Environment;
@@ -13,19 +17,28 @@ import android.os.SystemClock;
 import android.speech.tts.Voice;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Chronometer;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.h.chad.alexaalarmclock.data.AlarmContract;
+import com.h.chad.alexaalarmclock.data.AlarmContract.AlarmEntry;
+
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -39,21 +52,27 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
  * Created by chad on 3/14/2017.
  */
 
-public class VoiceRecorder extends AppCompatActivity{
+public class VoiceRecorder extends AppCompatActivity
+    implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private MediaRecorder voiceRecorder;
     private MediaPlayer testRecording;
     private Button startRecording;
     private Button playBack;
-    private EditText recordingName;
-    private Chronometer recordingLength;
     private String fileSavePath = null;
+
+    private Chronometer recordingLength;
+
     private LinearLayout mTime_button;
-    private TextView mHours;
-    private TextView mMinutes;
+
     protected int mHoursForDB;
     protected int mMinutesForDB;
     private Button mSaveButton;
+    private Uri mCurrentAlarmUri;
+
+    private EditText recordingName;
+    private TextView mHours;
+    private TextView mMinutes;
 
     public final static String LOG_TAG = VoiceRecorder.class.getSimpleName();
     public final static int REQUEST_PERMISSION_CODE = 1;
@@ -74,6 +93,8 @@ public class VoiceRecorder extends AppCompatActivity{
         playRecording();
         timeClicked();
         saveButtonClicked();
+
+
     }
 
 
@@ -190,6 +211,11 @@ public class VoiceRecorder extends AppCompatActivity{
                 result1 == PackageManager.PERMISSION_GRANTED;
     }
 
+    /*
+    * settings for the MediaRecoder()
+    * https://developer.android.com/guide/topics/media/mediarecorder.html
+    *help from StackOverflow http://stackoverflow.com/questions/6600364/android-mediarecorder-getmaxamplitude-always-returns-0-on-lg-optimus/33992633#33992633
+    * */
     public void mediaRecorder() {
         voiceRecorder = new MediaRecorder();
         voiceRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -227,13 +253,81 @@ public class VoiceRecorder extends AppCompatActivity{
     private void saveButtonClicked() {
         mHoursForDB = Integer.parseInt(mHours.getText().toString().trim());
         mMinutesForDB = Integer.parseInt(mMinutes.getText().toString().trim());
+        Arrays.toString(daysOfTheWeek());
+
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO (NEXT) save the data to the database
+                ContentValues values = new ContentValues();
+                int duration = Toast.LENGTH_SHORT;
+                Context c = VoiceRecorder.this;
+
+
             }
         });
 
     }
 
+    public int[] daysOfTheWeek(){
+        int[] days = new int[7];
+        CheckBox mon = (CheckBox) findViewById(R.id.cb_monday);
+        CheckBox tue = (CheckBox) findViewById(R.id.cb_tuesday);
+        CheckBox wed = (CheckBox) findViewById(R.id.cb_wednesday);
+        CheckBox thu = (CheckBox) findViewById(R.id.cb_thursday);
+        CheckBox fri = (CheckBox) findViewById(R.id.cb_friday);
+        CheckBox sat = (CheckBox) findViewById(R.id.cb_saturday);
+        CheckBox sun = (CheckBox) findViewById(R.id.cb_sunday);
+        days[0] = (mon.isChecked()) ? 1 : 0;
+        days[1] = (tue.isChecked()) ? 1 : 0;
+        days[2] = (wed.isChecked()) ? 1 : 0;
+        days[3] = (thu.isChecked()) ? 1 : 0;
+        days[4] = (fri.isChecked()) ? 1 : 0;
+        days[5] = (sat.isChecked()) ? 1 : 0;
+        days[6] = (sun.isChecked()) ? 1 : 0;
+        return days;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String projection[] = {
+                AlarmEntry._ID,
+                AlarmEntry.USER_DESCRIPTION,
+                AlarmEntry.FILE_NAME,
+                AlarmEntry.ALARM_ACTIVE,
+                AlarmEntry.ALARM_HOUR,
+                AlarmEntry.ALARM_MINUTE,
+                AlarmEntry.ALARM_DAYS
+        };
+        return new CursorLoader(this, mCurrentAlarmUri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if(data == null || data.getCount() < 1){return;}
+        if(data.moveToFirst()){
+
+            int user_descriptionColumnIndex = data.getColumnIndex(AlarmEntry.USER_DESCRIPTION);
+            int file_nameColumnIndex = data.getColumnIndex(AlarmEntry.FILE_NAME);
+            int alarm_activeColumnIndex = data.getColumnIndex(AlarmEntry.ALARM_ACTIVE);
+            int alarm_hourColumnIndex = data.getColumnIndex(AlarmEntry.ALARM_HOUR);
+            int alarm_minuteColumnIndex = data.getColumnIndex(AlarmEntry.ALARM_MINUTE);
+            int days_of_weekColumnIndex = data.getColumnIndex(AlarmEntry.ALARM_DAYS);
+
+            String user_Description = data.getString(user_descriptionColumnIndex);
+            String fileName = data.getString(file_nameColumnIndex);
+            int isAlarmActive = data.getInt(alarm_activeColumnIndex);
+            int alarmHour = data.getInt(alarm_hourColumnIndex);
+            int alarmMinute = data.getInt(alarm_minuteColumnIndex);
+            String alarmDaysOfWeek = data.getString(days_of_weekColumnIndex);
+
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        recordingName.setText("");
+        mHours.setText("");
+        mMinutes.setText("");
+
+    }
 }
